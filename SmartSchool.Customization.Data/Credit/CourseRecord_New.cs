540d77186f241391de6e759250ea27ed652ef1d4
dylan.huang <dylan.huang@ischool.com.tw>
2014-10-13 12:15:42 +0800

    調整核心模組_支援學分數可輸入小數點_1013

---
+++ SmartSchool.Customization.Data/Credit/CourseRecord_New.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartSchool.Customization.Data
+{
+     /// <summary>
+     /// 課程紀錄(New)
+     /// </summary>
+     public interface CourseRecord_New : CourseRecord
+     {
+          /// <summary>
+          /// 學分數
+          /// </summary>
+          decimal CreditDec
+          {
+               get;
+          }
+     }
+}
